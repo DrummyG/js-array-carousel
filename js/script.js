6756d0ac01eb2a333738350ad32bbe8225de5fa2
DrummyG <balestrinodavide@gmail.com>
2022-03-30 15:32:06 +0200

    lavoro sullo slide

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const slider = document.getElementById('slider');
+const img = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
+for (i=0; i<img.length; i++){
+    slider.innerHTML += `
+    <div class="box">
+        <img src="${img[i]}" alt="">
+    </div>`
+}
+
+for (i=0; i<img.length; i++){
+    slider.innerHTML += `
+    <div class="aside">
+        <img src="${img[i]}" alt="">
+    </div>`
+}
+
+const box = document.getElementsByClassName('box')
+const down = document.querySelector('.fa-angle-down')
+let counter = 0
+down.addEventListener('click', function(){
+    box[counter].classList.remove('show')
+    if (counter == 5){
+        counter = -1
+    }
+    counter = counter + 1
+    box[counter].classList.add('show')
+})
+
+const up = document.querySelector('.fa-angle-up')
+up.addEventListener('click', function(){
+    box[counter].classList.remove('show')
+    if (counter == 5){
+        counter = 1
+    }
+    counter = counter - 1
+    box[counter].classList.add('show')
+})
+
